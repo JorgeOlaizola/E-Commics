9e4e18d139434812e062c48fe2b6cdd17753f491
Mariano Avancini <avancini16@gmail.com>
2021-07-06 23:19:41 -0300

    categories routes

--- server/controllers/categories.controllers.js
+++ server/controllers/categories.controllers.js
@@ -5,7 +5,6 @@ const categories = {};
 categories.categoriesController = async (req, res) => {
     const categoriesNames = await Category.find();
     res.send(categoriesNames);
-    
 } 
 
 module.exports = categories;

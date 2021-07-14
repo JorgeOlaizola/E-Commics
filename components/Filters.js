ee9e508dd0a2bc8f7f642ae52fd1af48fc025f93
jeremiasfolgado <jeremias.folgado@gmail.com>
2021-07-14 10:10:07 -0300

    add page products per user

--- components/Filters.js
+++ components/Filters.js
@@ -60,8 +60,8 @@ width: 100%;
 `
 
 
-const Filters = () => {
-    
+const Filters = ({userId}) => {
+    //console.log("aca en filter", userId.user)
     const dispatch = useDispatch()
     const categories = useSelector(state => state.category.categories)
     const filters = useSelector(state => state.product.filters)
@@ -85,6 +85,10 @@ const Filters = () => {
         let newFilters = {
             ...filters
         }
+        //userId && {...newFilters, user: userId.user}
+        if(userId) newFilters = {...newFilters, user: userId.user}
+        
+
         if(!parseInt(filters.price.start)){
             newFilters.price.start = 0
         }

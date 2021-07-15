17856319f137f2e7ebcd7e166e35a111e03456e2
MatiasCavallo <65646120+MatiasCavallo@users.noreply.github.com>
2021-07-15 16:28:13 -0300

    fix env and SingUp load image

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@
 
 module.exports = {
   reactStrictMode: true,
+  env:{
+    CLOUDINARY_PRESET: "pxwajoby",
+    MONGODB_URI: 'mongodb://admin-ecommics:kj9RZdNdYjrmMeWO@cluster0-shard-00-00.n17hy.mongodb.net:27017,cluster0-shard-00-01.n17hy.mongodb.net:27017,cluster0-shard-00-02.n17hy.mongodb.net:27017/ecommics?authSource=admin&replicaSet=atlas-1v5dgy-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+    CLOUDINARY_URL  : "https://api.cloudinary.com/v1_1/ddrhpcrzz/image/upload"
+  }
 }
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
-const { USER_HOST, USER_PASS, USER_DB } = process.env
-const MONGODB_URI = `mongodb://${USER_HOST}:${USER_PASS}@cluster0-shard-00-00.n17hy.mongodb.net:27017,cluster0-shard-00-01.n17hy.mongodb.net:27017,cluster0-shard-00-02.n17hy.mongodb.net:27017/${USER_DB}?authSource=admin&replicaSet=atlas-1v5dgy-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`
+
+const MONGODB_URI = process.env.MONGODB_URI
 
 if (!MONGODB_URI) {
   throw new Error(

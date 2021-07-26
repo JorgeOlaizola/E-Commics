9b63180b46687ac20ad78aea29aa53223be0a9c1
Mariano Avancini <avancini16@gmail.com>
2021-07-25 23:33:34 -0300

    Newsletter routes

---
+++ server/models/Newsletter.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const newsletterSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true
+	},
+    content: {
+        type: String, 
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    } 
+});
+
+module.exports = mongoose.models.Newsletter || mongoose.model('Newsletter', newsletterSchema);

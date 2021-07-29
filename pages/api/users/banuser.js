839e0112171abf384b1767baefbea6697467986c
Jorge Olaizola <jorgelolaizola@hotmail.com>
2021-07-29 17:58:02 -0300

    Update rating

--- pages/api/users/banuser.js
+++ pages/api/users/banuser.js
@@ -1,6 +1,9 @@
 import dbConnect from '../../../utils/dbConnect'
 import nextConnect from 'next-connect'
 import User from '../../../server/models/User'
+import Order from '../../../server/models/Order'
+import Question from '../../../server/models/Question'
+import Product from '../../../server/models/Product'
 
 export default nextConnect()
 
@@ -9,11 +12,74 @@ export default nextConnect()
         await dbConnect()
 
         const { userId, status } = req.body
-        
-        const user = await User.findById(userId).exec()
-        user.status = status
-        await user.save()
-        return res.json({ success_msg: 'Usuario actualizado con éxito' })
+        let user = await User.findById(userId).exec()
+        if(status === 'banned'){
+            user.status = status
+
+            //----------------------------------------------------
+            //User interactions Handler
+            //----------------------------------------------------
+            //-----DELETING QUESTIONS 
+            // let userQuestions = await Question.find({}).where({ user: userId }).exec()
+            // userQuestions && userQuestions.length > 0 && userQuestions.map(async (q) => {
+            //     return(
+            //         await Question.findByIdAndDelete(q._id)
+            //     )
+            // })
+
+            //-----UPDATING PRODUCT STATUS
+            // let userProducts = await Product.find({}).where({ user: userId }).exec()
+            // userProducts && userProducts.length > 0 && userProducts.map(async (p) => {
+            //     return(
+            //         await Product.findByIdAndUpdate(p._id, { status: 'inactive' })
+            //     )
+            // })
+
+            //-----BUYS AND SELL NOTIFICATION HANDLER
+            let userBuys = await Order.find({}).where({ buyer: userId }).exec()
+
+            let userSells = await Order.find({}).where({ seller: userId }).exec()
+
+
+            await user.save()
+            return res.json({ success_msg: 'Usuario baneado con éxito'})
+
+        }
+        if(status === 'active'){
+            user.status = status
+
+            //----------------------------------------------------
+            //User interactions Handler
+            //----------------------------------------------------
+            //---UPDATING PRODUCT STATUS
+            // let userProducts = await Product.find({}).where({ user: userId }).exec()
+            // userProducts && userProducts.length > 0 && userProducts.map(async (p) => {
+            //     return(
+            //         await Product.findByIdAndUpdate(p._id, { status: 'active' })
+            //     )
+            // })
+
+            //-----BUYS AND SELL NOTIFICATION HANDLER
+            let userBuys = await Order.find({}).where({ buyer: userId }).exec()
+
+            let userSells = await Order.find({}).where({ seller: userId }).exec()
+
+            //User notification handler
+            if(user){
+                const notification = {
+                    img: 'https://res.cloudinary.com/jorgeleandroolaizola/image/upload/v1627578321/Notifications%20eccomics/Unban_xywe6a.png',
+                    content: `Se habilitó nuevamente tu cuenta de E-Commics. Recuerda leer los términos y condiciones para evitar futuros inconvenientes`,
+                    link: `/help/terms-and-conditions`
+                }
+                user.notifications.unshift(notification)
+                if(user.notifications.length > 5){
+                    user.notifications.pop()
+                }
+            }
+            await user.save()
+            return res.json({ success_msg: 'Usuario desbaneado con éxito' })
+        }
+        return res.json({ error_msg: 'Algo salió mal'})
     }
     catch(error){
         console.log(error)

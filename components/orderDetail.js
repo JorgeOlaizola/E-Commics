7da58f47ebc7e2f3ee6c68ca4a9c0c209a2dfcbd
gallegoagustin <gallegoagustin7@gmail.com>
2021-07-28 23:51:00 -0300

    Added styles to order details

--- components/orderDetail.js
+++ components/orderDetail.js
@@ -6,18 +6,42 @@ import { updateOrders } from '../store/actions/normalUsersActions'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import OrderDetailProduct from './OrderDetailProduct'
+import { GradientBorder } from './globalStyle'
 
 const OrderDetailConteiner = styled.div`
-width: 80%;
-height: 700px;
-border: 1px solid grey;
-border-radius: 1rem 1rem;
+width: 100%;
+min-height: 100vh;
 display: flex;
 flex-direction: column;
-justify-content: center;
+justify-content: top;
 align-items:center;
 `
 
+const StyledTop = styled.div`
+width: 80%;
+height: 200px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+text-align: center;
+background-color: #c0f0ff;
+border-top: 1px solid #000;
+border-right: 1px solid #000;
+border-left: 1px solid #000;
+`
+
+const StyledWhiteBox = styled.div`
+width: 80%;
+display: flex;
+flex-direction: column;
+align-items:center;
+justify-content: center;
+padding: 20px 0;
+border-right: 1px solid #000;
+border-left: 1px solid #000;
+border-bottom: 1px solid #000;
+`
 
 const ProductOrderConteiner = styled.div`
 width: 70%;
@@ -42,11 +66,27 @@ align-items: center;
 padding: 5px;
 `
 
+const StyledButton = styled.button`
+border: 2px solid #000;
+font-size: 18px;
+background-color: transparent;
+padding: 5px;
+cursor: pointer;
+&:hover{
+    box-shadow: 4px 2px 2px rgb(0,0,0,0.5);
+}
+`
+
+const Space = styled.div`
+height: 10px;
+`
+
 const OrderDetailComponent = ({ orderProps }) => {
     
     const router = useRouter()
     // const userData = useSelector(state => state.user.userData.user)
     const [userData, setUserData] = useState('')
+    const [userRole, setUserRole] = useState("")
     if (typeof window !== "undefined" && userData === ''){
         localStorage.getItem("sessionSaved") ? setUserData(JSON.parse(localStorage.getItem("sessionSaved"))) : null 
     }
@@ -54,46 +94,71 @@ const OrderDetailComponent = ({ orderProps }) => {
         if(!userData.user) {
             router.push("/")
         }
+        if(userData.user.nickname === orderProps.buyer.nickname) {
+            setUserRole("buyer");
+        }else{
+            setUserRole("seller");
+        }
     }, [userData])
     const dispatch = useDispatch()
     let total = 0
 
+    const buyerDisplay = {
+        name: orderProps.buyer.name,
+        mainTitle: `Este es el resumen de tu compra a: ${orderProps.seller.name} ${orderProps.seller.surname} (${orderProps.seller.nickname})`,
+        type: 'compra'
+    }
+
+    const sellerDisplay = {
+        name: orderProps.seller.name,
+        mainTitle: `Este es el resumen de tu venta a: ${orderProps.buyer.name} ${orderProps.buyer.surname} (${orderProps.buyer.nickname})`,
+        type: 'venta'
+    }
  
     return (
-        // AAAA Info de contacto --> orderProps.buyer === userData.user.id ? <div>Info del vendedor></div> : ordersProps.seller === userData.user.id ? <div>Info del comprador</div> : null
         <OrderDetailConteiner>
-            <p>Merchant Order: {orderProps.MerchantOrder}</p>
-            <p>Pago: {orderProps.Payment}</p>
-            <p>Comprador: {orderProps.buyer.name} {orderProps.buyer.surname} ({orderProps.buyer.nickname})</p>
-            <p>Vendedor: {orderProps.seller.name} {orderProps.seller.surname} ({orderProps.seller.nickname})</p>
-            <p>Estado: {orderProps.status}</p>
-            <p>Productos: {orderProps.products && orderProps.products.length > 0 ?
-            <span>
-            {
-            orderProps.products.map(p => {                
-                total += (p.quantity * p.unit_price)
-                return (
-                    <OrderDetailProduct key={p._id} p={p} orderProps={orderProps}/>
+            <StyledTop>
+                <h1>¡Hola {userRole === "buyer" ? buyerDisplay.name : sellerDisplay.name}!</h1>
+                <h2>{userRole === "buyer" ? buyerDisplay.mainTitle : sellerDisplay.mainTitle}</h2>
+            </StyledTop>
+            <StyledWhiteBox>
+                <p><strong>Número de orden de Mercado Pago:</strong> {orderProps.MerchantOrder}</p>
+                <p><strong>Número de pago:</strong> {orderProps.Payment}</p>
+                <p><strong>Estado de la {userRole === "buyer" ? buyerDisplay.type : sellerDisplay.type}:</strong> {orderProps.status}</p>
+                <br/><br/>
+                <h3><strong>Artículos</strong></h3>
+                <br/>
+                {orderProps.products && orderProps.products.length > 0 ?
+                <span>
+                {
+                    orderProps.products.map(p => {                
+                        total += (p.quantity * p.unit_price)
+                        return (
+                            <OrderDetailProduct key={p._id} p={p} orderProps={orderProps}/>
 
-                )
-            })}
-            Monto final: {total}$
-            </span>
-            :
-            'No hay productos'
-            }</p>
+                        )
+                    })}
+                    <p style={{textAlign:"center",marginTop:"10px"}}><strong>Monto final:</strong> ${total}</p>
+                    </span>
+                    :
+                    'No hay productos'
+                }
+            </StyledWhiteBox>
+            <Space/>
             { userData.user && userData.user.id === orderProps.seller._id &&  orderProps.status === 'Pago realizado' ? 
-            <button onClick={() => {
-                dispatch(updateOrders(orderProps._id, orderProps.status, userData.user.id))
-                alert('Estado actualizado')
-                router.push(`/orderDetail/${orderProps._id}`)
-                }}>Despaché este producto</button> : null}
+                    <StyledButton onClick={() => {
+                        dispatch(updateOrders(orderProps._id, orderProps.status, userData.user.id))
+                        alert('Estado actualizado')
+                        router.push(`/orderDetail/${orderProps._id}`)
+                        }}>Ya despaché este producto</StyledButton>
+                : null
+            }
             { userData.user && userData.user.id === orderProps.buyer._id &&  orderProps.status === 'En proceso de entrega' ? 
-            <button onClick={() => {
+            <StyledButton onClick={() => {
                 dispatch(updateOrders(orderProps._id, orderProps.status, userData.user.id))
                 alert('Estado actualizado')
                 router.push(`/orderDetail/${orderProps._id}`)
-                }}>Recibí este producto</button> : null}
+                }}>Recibí este producto</StyledButton> : null}
         </OrderDetailConteiner>
     )
 }

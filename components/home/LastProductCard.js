4d25d2dce9f0379730e6b0a3fc10db890b175cf7
jeremiasfolgado <jeremias.folgado@gmail.com>
2021-07-29 23:52:10 -0300

    finish carrousel last post

--- components/home/LastProductCard.js
+++ components/home/LastProductCard.js
@@ -1,25 +1,35 @@
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import {StyledLink} from '../globalStyle'
+import Link from 'next/link'
+import { FaLink } from "react-icons/fa";
 
 const MainContainer = styled.div`
 width:140px;
 height:300px;
 
-border:1px solid black;
+border:1px solid ${(props) => props.theme.colorLevel4};
+&:hover {
+    box-shadow: 0 0 20px rgba(33,33,33,.2);
+    ${'' /* transform: scale(1.1); */}
+    background: ${(props) => props.theme.backgroundLevel2}
+}
+@media (max-width: 320px) {
+    margin: 0.6rem 0;
+    }
 
 `
 const ImgContainer= styled.div`
 width:100%;
 height: 140px;
-border-bottom: 1px solid black;
+border-bottom: 1px solid ${(props) => props.theme.colorLevel4};
 display:flex;
 justify-content:center;
 `
 
 const ImageStyled = styled.img`
 max-width: 100%;
-max-height: 160px;
+max-height: 140px;
 `
 const PriceTitle = styled.h3`
 font-size:1.4rem;
@@ -29,9 +39,35 @@ margin-top:20px;
 `
 const InfoLinkContainer = styled.div`
 width:80%;
+height:140px;
 margin:auto;
 display:flex;
 flex-direction: column;
+justify-content: space-around;
+`
+const StyledButton = styled(StyledLink)`
+margin-bottom: 5px;
+color: #FF0000;
+display: inline;
+padding-right: 2px;
+transition: 0.2s;
+
+&:hover {
+    color: #E10000;
+    padding-right: 7px;
+    transition: 0.2s;
+}
+`
+
+const ArrowSpan = styled.span`
+display: inline;
+padding-left: 2px;
+transition: 0.2s;
+color: #FF0000;
+&:hover {
+    padding-left: 7px;
+    transition: 0.2s;
+}
 `
 
 const LastPostedProductCard = ({product})=>{
@@ -44,10 +80,18 @@ const LastPostedProductCard = ({product})=>{
             </ImgContainer>
             <InfoLinkContainer>
                 <PriceTitle>$ {product.price}</PriceTitle>
-                <StyledLink>
-                    {product.title}
-                </StyledLink>
+                    <span>
+                            {product.title}
+                    </span>
+                
+              
+                       
+                <div style={{marginTop:'5px'}}>
 
+                <Link  href={'/detail/[productDetail]'} as={`/detail/${product._id}` } passHref>
+                                <StyledButton >ver detalle </StyledButton>
+                            </Link><ArrowSpan>→</ArrowSpan>
+                </div>
             </InfoLinkContainer>
 
         </MainContainer>

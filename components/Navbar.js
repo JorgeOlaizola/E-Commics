27d15b53f9646787641f781df4491a2100db693f
9 <a9@fyc-pc4.ferias.local>
2021-09-09 16:25:26 -0300

    about us to the footer

--- components/Navbar.js
+++ components/Navbar.js
@@ -135,9 +135,6 @@ const Nav = ({ theme}) => {
                             </StyledResponsive>
                         </StyledLogoResponsive>
                     </Link>
-                    <Link href="/about" passHref >
-                        <StyledLink className={router.pathname == '/about' ? "active" : ""}>Sobre nosotros</StyledLink>
-                    </Link>
                     <Link href="/search" passHref >
                         <StyledLink className={router.pathname == '/search' ? "active" : ""}>Explorar</StyledLink>
                     </Link>
